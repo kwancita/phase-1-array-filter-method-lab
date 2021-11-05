21956e62796c11411df355027024fb82b7189c4f
Wasanta <kwan_u_day@hotmail.com>
2021-11-04 23:06:35 -0400

    commit tree 9f63c07f56fbae9bea6e94a5d0a924f804feabf0

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution here
+
+// function findMatching(drivers,string){
+    
+//     const allName = drivers.filter(obj=>obj.name === string)
+//     for (const findDriver of drivers){
+//         if (allName === undefined){
+//             return [];
+//         }else if(allName ){
+
+//         }
+//     }
+// }
+
+
+
+  function findMatching(array,string){
+      return array.filter(function (nameMatch){
+          return nameMatch.toLowerCase()===string.toLowerCase();
+      }
+    )
+  }
+
+  function fuzzyMatch(array,letters){
+    return array.filter(matchName => 
+        matchName.indexOf(letters)===0);
+  }
+
+  function matchName (array,letters){
+      const match = array.filter(obj => obj.name === letters)
+      return match
+  }
+
+
+
